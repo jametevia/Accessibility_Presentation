8018828e3f3c0d60f90146834112bb04a7583faa
Jacob <ja.metevia@gmail.com>
2021-06-16 15:03:08 -0700

    sized image and select

--- Client/Components/App.js
+++ Client/Components/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import NonAccessibleNav from './nonAccessibleNav.js';
 import javascriptImage from '../../Images/javascript.jpg';
+import cssImage from '../../Images/css.jpeg';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {color: 'purple'};
+    this.state = {nonAccessibleImage: javascriptImage};
    }
   render() {
     return (
       <div>
         <NonAccessibleNav />
-        <img id="nonAccessibleJSImage" src={javascriptImage}></img>
+        <img id="nonAccessibleJSImage" src={this.state.nonAccessibleImage}></img>
         <div></div>
       </div>
 
--- Client/Components/nonAccessibleNav.js
+++ Client/Components/nonAccessibleNav.js
@@ -3,13 +3,9 @@ import React from 'react';
 const nonAccessibleNav = (props) => {
   return (
     <div>
-      <select>
+      <select className={"nonAccessibleSelect"}>
         <option value="javascript">javascript</option>
         <option value="css">css</option>
-      </select>
-      <select>
-        <option value="express">express</option>
-        <option value="react">react</option>
       </select>
     </div>
   )
