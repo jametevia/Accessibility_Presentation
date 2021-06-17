d6971874804f79aae9ba07917a837680d772543a
Jacob <ja.metevia@gmail.com>
2021-06-16 17:01:18 -0700

    added functionality to take you to link on click

--- Client/Components/App.js
+++ Client/Components/App.js
@@ -1,19 +1,57 @@
 import React from 'react';
 import NonAccessibleNav from './nonAccessibleNav.js';
-import javascriptImage from '../../Images/javascript.jpg';
-import cssImage from '../../Images/css.jpeg';
+import javascriptImage from '../../Images/javascript.png';
+import cssImage from '../../Images/css.png';
+import reactImage from '../../Images/react.jpeg';
+import expressImage from '../../Images/express.png';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {nonAccessibleImage: javascriptImage};
+    this.imageChange = this.imageChange.bind(this);
+    this.redirect = this.redirect.bind(this);
    }
+
+
+  imageChange (e) {
+    if (e.target.value === 'css') {
+      this.setState({nonAccessibleImage: cssImage})
+    }
+    if (e.target.value === 'react') {
+      this.setState({nonAccessibleImage: reactImage})
+    }
+    if (e.target.value === 'express') {
+      this.setState({nonAccessibleImage: expressImage})
+    }
+    if (e.target.value === 'javascript') {
+      this.setState({nonAccessibleImage: javascriptImage})
+    }
+   }
+
+   redirect () {
+     if (this.state.nonAccessibleImage === cssImage) {
+      window.open('https://developer.mozilla.org/en-US/docs/Web/CSS','_blank')
+     }
+     if (this.state.nonAccessibleImage === javascriptImage) {
+       window.open('https://www.javascript.com/', '_blank');
+     }
+     if (this.state.nonAccessibleImage === reactImage) {
+      window.open('https://reactjs.org/', '_blank');
+    }
+    if (this.state.nonAccessibleImage === expressImage) {
+      window.open('http://expressjs.com/', '_blank');
+    }
+
+   }
+
   render() {
     return (
       <div>
         <div className="NonAccessibleComponentHeader">Inaccessible Component</div>
-        <NonAccessibleNav />
-        <img id="nonAccessibleJSImage" src={this.state.nonAccessibleImage}></img>
+        <div className="learn">What would you like to learn?</div>
+        <NonAccessibleNav imageChange={this.imageChange}/>
+        <img id="nonAccessibleJSImage" src={this.state.nonAccessibleImage} onClick={this.redirect}></img>
         <div></div>
       </div>
 
--- Client/Components/nonAccessibleNav.js
+++ Client/Components/nonAccessibleNav.js
@@ -3,11 +3,13 @@ import React from 'react';
 const nonAccessibleNav = (props) => {
   return (
     <div className={"nonAccessibleSelect"}>
-      <select className="nonAccessibleJS">
-        <option value="javascript">javascript</option>
-        <option value="css">css</option>
-      </select>
-      <select className="nonAccessibleExpress">
+      <label className="nonAccessibleJSLabel">&nbsp;<select className="nonAccessibleJS" onChange={props.imageChange}>
+          <option value="SelectOne">Select One</option>
+          <option value="javascript">javascript</option>
+          <option value="css">css</option>
+        </select></label>
+      <select className="nonAccessibleExpress" onChange={props.imageChange}>
+        <option value="SelectOne">Select One</option>
         <option value="express">express</option>
         <option value="react">react</option>
       </select>
